ea3051b45aeeaea36126efffd4b568d7d3691b25
Railson25 <railsonsantiago123@gmail.com>
2023-09-16 21:57:24 -0300

    <feat>: advertising component

---
+++ components/advertising.tsx
@@ -0,0 +1,17 @@
+import { CloseIcon } from "./icons/close-icon"
+
+export const Advertising = () => {
+    return(
+        <div className="flex py-2">
+            <div className="w-full flex justify-start sm:justify-center">
+                <p className=" font-normal text-[14px] max-sm:text-[10px] max-md:text-[12px]">
+                    Sign up and get 20% off to your first order.
+                    <span className="pl-2 font-medium hover:underline">Sign Up Now</span>
+                </p>
+            </div>
+            <div className=" max-sm:hidden fill-white">
+                <CloseIcon />
+            </div>
+        </div>
+    )
+}
