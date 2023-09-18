6e492c9f26df938404ba48831febed5157f05aa0
Railson25 <railsonsantiago123@gmail.com>
2023-09-18 11:40:53 -0300

    <style> Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import Link from "next/link"
 import { Advertising } from "./advertising"
-import { CloseIcon } from "./icons/close-icon"
+import { Input } from "./Input"
+import { CarIcon } from "./icons/car-icon"
+import { PersonIcon } from "./icons/person-icon"
+import { ArrowDown } from "./icons/arrow-down"
+import { SearchIcon } from "./icons/search-icon"
+
 
 export const Navbar = () => {
+
     return(
-        <div className="w-ful px-24 py-2 flex flex-col max-sm:px-10 bg-blue-400 justify-start ">
+        <div className="w-ful px-24 py-2 flex flex-col max-sm:px-10 justify-start ">
             <Advertising />
-            <div className="flex items-center justify-between">
-                <h1 className="uppercase font-bold text-[32px]">
+            <div className="flex items-center justify-between py-6">
+                <h1 className="uppercase font-bold text-[32px] max-lg:text-[26px]">
                     SHOP.CO
                 </h1>
-                <nav className="flex px-10 max-md:hidden">
-                    <Link href='/' className="flex text-[16px] font-normal">
+                <nav className="
+                flex gap-x-6 px-10 max-lg:text-[12px] max-md:hidden text-[16px] font-normal max-lg:gap-x-4">
+                    <Link href='/' className="flex items-center gap-x-1 fill-white">
                         Shop
-                        <CloseIcon />
+                        <ArrowDown />
                     </Link>
                     <Link href='/'>
                         Shop
@@ -26,11 +35,14 @@ export const Navbar = () => {
                     </Link>
                 </nav>
 
-                <input className="max-md:hidden"/>
+                <Input />
 
-                <div className="flex pl-10 ">
-                    <CloseIcon />
-                    <CloseIcon />
+                <div className="flex pl-10 max-lg:pl-4 gap-x-3 fill-white">
+                    <div className="md:hidden w-6 h-6">
+                        <SearchIcon />
+                    </div>
+                    <CarIcon  />
+                    <PersonIcon />
                 </div>
 
             </div>
